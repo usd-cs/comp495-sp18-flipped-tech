705b4cc3eaf05d5d73d5466f192041f2a6f71ce6
Erick Perez <erickjperez@sandiego.edu>
2018-03-10 13:09:29 -0800

    Resolves #9
    Resolves issue 9 of basic server setup

--- FlippedTech/app.js
+++ FlippedTech/app.js
@@ -1,26 +1,40 @@
+//This file is the root to our server. It starts it up and creates the routes to different webpages
+//once we begin to get further in the project, it will need to be refactored
 
+
+//below are declarations for npm packages that we will be using
+//express allows us to route to different pages
 var express = require("express");
 var app = express();
+//body parser allows us to take input from pages and get it as a usable string
 var bodyParser = require("body-parser");
 
 //allows ejs files in views folder to locate css files in public
 app.use(express.static("public"));
 //allows for information sent from pages to be rendered and used
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(express.static(path.join(__dirname, 'views')));
 
+
+//allows for code minimization
+//when giving a file, just give name. Does not need .ejs
 app.set("view engine", "ejs");
 
+
+//the following .get and .post functions create the routes to different
+//pages and we can then pass data in and out of the ejs files
+//req and res found as paramters are typicala names for routes and their actions
+//req is for request and is used when info is being submitted through a page
+//res is a response and is used when responding and routing to a page
 app.get("/", function(req, res){
     res.render("homePage");
 });
 
-app.post("/newComment", function(req, res){
-    res.redirect("/lecturePage");
-});
-
 app.get("/lecturePage", function(req, res){
     res.render("lecturePage");
+});
+
+app.post("/newComment", function(req, res){
+    res.redirect("/lecturePage");
 });
 
 app.get("/lectureUpload", function(req, res){
@@ -38,7 +52,7 @@ app.get("*", function(req, res){
 });
 
 //tell express to listen for requests(start server)
-//process.env.PORT is used for cloud9. When programming on own comp, give it a port number
+//this sets up the server on localhost 3000
 app.listen(3000, function(){
     console.log("server has started");
 });
