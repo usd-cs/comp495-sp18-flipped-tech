c208334127cd4e1fe025a101e0511a77b1dd55bf
Jack Miller <jmmiller775@gmail.com>
2018-04-04 19:17:10 -0700

    figured out accessing YT api with key

--- FlippedTech/flippedtech/src/Youtube.js
+++ FlippedTech/flippedtech/src/Youtube.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
 
 //http request is not working at the moment
-//var url = 'https://www.googleapis.com/youtube/v3/search?key={AIzaSyBRDl7uhaoKWR5xPHAtdDiU9ROcOj7EGcY}&channelId={UCXgGY0wkgOzynnHvSEVmE3A}&part=snippet,id&order=date&maxResults=5'
+var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&videoCaption=closedCaption&key=AIzaSyBRDl7uhaoKWR5xPHAtdDiU9ROcOj7EGcY'
 
 //Just a base video embed in case I cannot figure out API
 class Youtube extends Component{
+
+	click(){
+		fetch('https://facebook.github.io/react-native/movies.json')
+			.then((response) => response.json())
+			.then((responseJson) => {
+				console.log(responseJson)
+				//return responseJson.movies;
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
 	render(){
-		console.log(url);
 		return(
 			<div>
 			<button>Get Youtube Videos</button>
 				<div>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/A71aqufiNtQ" frameborder="0"
-						 allow="autoplay; encrypted-media" allowfullscreen>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/A71aqufiNtQ" frameBorder="0"
+						 allow="autoplay; encrypted-media" allowFullScreen>
 					</iframe>
 				</div>
 			</div>
