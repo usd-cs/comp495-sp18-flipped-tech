dd818e6492710edb352875e8082cde18699f909e
Jack Miller <jmmiller775@gmail.com>
2018-04-04 19:30:16 -0700

    Http request returns a json object

--- FlippedTech/flippedtech/src/Youtube.js
+++ FlippedTech/flippedtech/src/Youtube.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 
-//http request is not working at the moment
+//URL to be used for the actual request
 var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&videoCaption=closedCaption&key=AIzaSyBRDl7uhaoKWR5xPHAtdDiU9ROcOj7EGcY'
 
-//Just a base video embed in case I cannot figure out API
 class Youtube extends Component{
+	constructor(props){
+		super(props);
 
-	click(){
-		fetch('https://facebook.github.io/react-native/movies.json')
+		this.state = {
+			resultyt: []
+		};
+		this.clicked = this.clicked.bind(this);
+	}
+
+	//Button click on page that actually runs the requesti
+	//Returns info as a Json object
+	clicked(){
+		fetch(url)
 			.then((response) => response.json())
 			.then((responseJson) => {
 				console.log(responseJson)
@@ -19,8 +28,10 @@ class Youtube extends Component{
 	}
 	render(){
 		return(
+			
+			//Just a base video embed in case I cannot figure out API
 			<div>
-			<button>Get Youtube Videos</button>
+			<button onClick={this.clicked}>Get Youtube Videos</button>
 				<div>
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/A71aqufiNtQ" frameBorder="0"
 						 allow="autoplay; encrypted-media" allowFullScreen>
