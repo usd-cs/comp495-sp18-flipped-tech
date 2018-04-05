7a5e61ed273f104ab77ae1753916d57c7c7fc435
Seth Nakanishi <snakanishi@sandiego.edu>
2018-04-04 20:13:18 -0700

    Added comments to make XML easier to understand

--- FlippedTech/flippedtech/src/SigninPage.js
+++ FlippedTech/flippedtech/src/SigninPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Signin from './Signin'
 import Logo from './Logo'
 
+//SigninPage element that will display all the components of a signin page
 const SigninPage = (props) => {
 	return (
 		<div>
