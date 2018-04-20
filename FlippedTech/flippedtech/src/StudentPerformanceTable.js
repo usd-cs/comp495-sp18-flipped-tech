cda75346c148a3baa9bd451e3981a99b8eeca445
Taylor Coury <tacoury@sandiego.edu>
2018-04-20 12:18:41 -0700

    Student table created with dummy data

--- FlippedTech/flippedtech/src/StudentPerformanceTable.js
+++ FlippedTech/flippedtech/src/StudentPerformanceTable.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 const response = {
 	studentData: [{
 		name: 'Taylor Coury', 
-		answers: [1, 1, 1, 1, 1]
+		answers: [1, 1, 1, 1, 1], 
+		grade: 100
 		}, {
 		name: 'Amanda Ennis', 
-		answers: [1, 1,  0, 1, 1]
+		answers: [1, 1,  0, 1, 1], 
+		grade: 80
 		}, {
 		name: 'Levi Pike', 
-		answers: [1, 0, 0, 0, 1]
+		answers: [1, 0, 0, 0, 1], 
+		grade: 40
 		}, {
 		name: 'Bob Marley', 
-		answers: [1, 0, 0, 1, 1]
+		answers: [1, 0, 0, 1, 1], 
+		grade: 60
 		}, {
 		name: 'Shelby Burns', 
-		answers: [0, 0, 0, 0, 0]
+		answers: [0, 0, 0, 0, 0], 
+		grade: 0
 	}]
 }
 const calculateMean = function(numbers) {
@@ -25,7 +31,7 @@ const calculateMean = function(numbers) {
 		for (i = 0; i < numbers.length; i += 1) {
 			total += numbers[i];
 		}
-		return total / numbers.length;	
+		return (total / numbers.length) * 100;	
 }
 
 class StudentPerformanceTable extends React.Component {
@@ -44,6 +50,9 @@ class StudentPerformanceTable extends React.Component {
 				const answerList = value.map(q=>q).join(", ")
 				return <span>{answerList}</span>
 			}
+			}, {
+			Header: 'Grade',
+			accessor: 'grade'
 		}]
 	
 		return (
