5cf08d5a0f9ee1a284e10fa2b4d8feee668c1d17
Taylor Coury <tacoury@sandiego.edu>
2018-05-15 21:27:04 -0700

    Create LectureBorder.js

---
+++ FlippedTech/flippedtech/src/AdminComponents/LectureBorder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/LectureUpload.css'
+
+const LectureBorder = (props) => {
+	return (
+		<div className='container'>
+				<img src={require('./images/chalkboard.jpg')} alt="LectureBorder" className='mw6'/>
+				<h1 className='LectureBorder centered'>Video Lecture Upload Form</h1>
+		</div>
+  );
+}
+
+export default LectureBorder;
